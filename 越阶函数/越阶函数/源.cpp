27a18fac1bd63452a7d98965652a17416a481ffd
rosemell <109337532+rosemell@users.noreply.github.com>
2022-10-22 22:55:03 +0800

    Add files via upload

---
+++ 越阶函数/越阶函数/源.cpp
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+int main()
+{
+    char x;
+    x = getchar();
+    printf("请输入你的分数");
+    switch (x)
+        case (int)'A':
+
+            return 0;
+}
